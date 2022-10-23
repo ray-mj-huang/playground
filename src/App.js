c07758b7cb445fac01d7d65a5a9dd0cfd577531e
ray-mj-huang <ray.mj.huang@gmail.com>
2022-10-23 22:42:55 +0800

    Try out some CSS animation effects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Playground
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        <h2>Without box-shadow transforms</h2>
+        <div className="thing">
+          <img src="https://pbs.twimg.com/media/CtwEYeHXgAAFWJA?format=jpg&name=large" alt="" />
+        </div>
+      </div>
+      <div>
+        <h2>With box-shadow transforms</h2>
+        <div className="thing threed">
+          <img src="https://pbs.twimg.com/media/CtwEYeHXgAAFWJA?format=jpg&name=large" alt="" />
+        </div>
+      </div>      
     </div>
   );
 }
